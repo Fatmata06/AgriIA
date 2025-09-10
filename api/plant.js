474666667e2c617859a9b3f67cd85e73b29e0dfa
Fatmata06 <nfatmata614@gmail.com>
2025-09-10 16:40:40 +0000

    6 commit

--- api/plant.js
+++ api/plant.js
@@ -16,7 +16,7 @@ export default async function handler(req, res){
     //     plant_details: ["common_names","disease","probability"]
     //   })
     // });
-    const response = await fetch('https://api.plant.id/api/v3/identification', {
+    const response = await fetch('https://api.plant.id/v3/identification', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
